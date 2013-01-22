7aa6f3a06105bd9987fd22d4b1504f3b06a3139a
Nikos Koutelidis <nikoutel@gmail.com>
2013-01-22 14:23:29 +0100

    cleand up Debugr; added eDbgLog

--- Debugr.php
+++ Debugr.php
@@ -1,39 +1,43 @@
 <?php
 
-//function __autoload($class_name) {
-//
-//    $class_file_path = str_replace('_', '/', $class_name) . '.php';
-//    // echo $class_file_path;
-//    require($class_name);
-//}
-
 Class Debugr {
 
     public static $debugVar;
     public static $debugText;
 
-    public static function eDbg($debugVar, $debugText = "") {
-        // if DBG or LOG...
-        self::eDbgScreen($debugVar, $debugText);
+    public static function getDebugVarType($debugVar) {
+        $type = ucwords(strtolower(gettype($debugVar)));
+        $type = str_replace(" ", "", $type);
+        $type = 'Type_Is' . $type;
+        return $type;
     }
 
+    public static function eDbg($debugVar, $debugText = "") {
+        // if DBG or LOG...
+        $out = 'Screen'; //?
+        $methodName = 'eDbg' . $out;
+        self::$methodName($debugVar, $debugText);
+    }
+    /** can be called directly **/
     public static function eDbgScreen($debugVar, $debugText = "") {
 
         self::$debugVar = $debugVar;
         self::$debugText = $debugText;
 
-        $type = ucwords(strtolower(gettype(self::$debugVar)));
-        $type = str_replace(" ", "", $type);
-        $type = 'Type_Is' . $type;
-
+        $type = self::getDebugVarType(self::$debugVar);
         $output = new Output_OutputScreen();
-
         $typeObj = new $type($output);
         return $typeObj;
     }
-
+    /** can be called directly **/
     public static function eDbgLog($debugVar, $debugText = "") {
-        
+        self::$debugVar = $debugVar;
+        self::$debugText = $debugText;
+
+        $type = self::getDebugVarType(self::$debugVar);
+        $output = new Output_OutputLog();
+        $typeObj = new $type($output);
+        return $typeObj;
     }
 
 }
