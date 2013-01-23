7ef2e592e8901de31cc52b59a66d437166ed989e
Nikos Koutelidis <nikoutel@gmail.com>
2013-01-23 13:35:02 +0100

    OutputOptions +file

---
+++ OutputOptions.php
@@ -0,0 +1,14 @@
+<?php
+
+final Class  OutputOptions {
+    
+    const Screen = 'Screen';
+    const Log = 'Log';
+    //const Mail = 'Mail';
+    
+    private function __construct() {
+        
+    }
+}
+
+?>
