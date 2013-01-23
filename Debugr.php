e662aea62b3fd534e7599124b7344f2014ebd45a
Nikos Koutelidis <nikoutel@gmail.com>
2013-01-23 14:43:37 +0100

    _eDbgOut(), cleaned up Debugr

--- Debugr.php
+++ Debugr.php
@@ -8,48 +8,41 @@ Class Debugr {
     public static function eDbg($debugVar, $debugText = "") {
 
 
-        $defaultOut = OutputOptions::Screen;
-        $methodName = 'eDbg' . $defaultOut;
-        self::$methodName($debugVar, $debugText);
+        $defaultOut = OutputOptions::Screen; // {Screen, Log, Mail}
+        self::_eDbgOut($debugVar, $debugText, $defaultOut);
     }
-
+    
     /** can be called directly * */
     public static function eDbgScreen($debugVar, $debugText = "") {
 
-        self::$debugVar = $debugVar;
-        self::$debugText = $debugText;
-
-        $type = self::getDebugVarType(self::$debugVar);
-
-        $output = new Output_OutputScreen();
-        $typeObj = new $type($output);
-        return $typeObj;
+        self::_eDbgOut($debugVar, $debugText, 'Screen');
     }
 
     /** can be called directly * */
     public static function eDbgLog($debugVar, $debugText = "") {
 
-        self::$debugVar = $debugVar;
-        self::$debugText = $debugText;
-
-        $type = self::getDebugVarType(self::$debugVar);
-
-        $output = new Output_OutputLog();
-        $typeObj = new $type($output);
-        return $typeObj;
+        self::_eDbgOut($debugVar, $debugText, 'Log');
     }
+    
+    /** can be called directly * */
     public static function eDbgMail($debugVar, $debugText = "") {
 
+        self::_eDbgOut($debugVar, $debugText, 'Mail');
+    }
+
+    private static function _eDbgOut($debugVar, $debugText, $out) {
+        
         self::$debugVar = $debugVar;
         self::$debugText = $debugText;
 
         $type = self::getDebugVarType(self::$debugVar);
+        $output = 'Output_Output'.$out;
 
-        $output = new Output_OutputMail();
+        $output = new $output;
         $typeObj = new $type($output);
         return $typeObj;
     }
-
+    
     public static function getDebugVarType($debugVar) {
         $type = ucwords(strtolower(gettype($debugVar)));
         $type = str_replace(" ", "", $type);
