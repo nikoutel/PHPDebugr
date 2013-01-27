d9ea5e545d62f04a01dcb216265ccb28ab4e07ed
Nikos Koutelidis <nikoutel@gmail.com>
2013-01-27 15:16:54 +0100

    working

--- Type/IsBoolean.php
+++ Type/IsBoolean.php
@@ -1,18 +1,11 @@
 <?php
 
-Class Type_IsBoolean extends Type_IsScalar {
+Class Type_IsBoolean extends Type_IsComposite {
 
     public $varAsString = ".";
 
     public function __construct($output) {
         parent::__construct();
-        if ($this->debugVar) {
-
-            $this->debugVarAsString = 'TRUE';
-        } else {
-
-            $this->debugVarAsString = 'FALSE';
-        }
         $this->send($output);
     }
 
--- Type/IsNull.php
+++ Type/IsNull.php
@@ -1,12 +1,9 @@
 <?php
 
-Class Type_IsNull extends Type_IsScalar { // Technicaly type NULL is not scalar but ...
+Class Type_IsNull extends Type_IsComposite { // Technicaly type NULL is not scalar but ...
 
     public function __construct($output) {
         parent::__construct();
-        if ($this->debugVar === null)
-            $this->debugVarAsString = 'NULL';
-
         $this->send($output);
     }
 
