f29e6723a85b6e9c4293d5597d302be7caa8909f
Nikos Koutelidis <nikoutel@gmail.com>
2018-11-28 12:49:04 +0100

    replaced autoloader with a namespace compatible autoloader

--- DebugrLoad.php
+++ DebugrLoad.php
@@ -25,15 +25,20 @@
 /**
  * Autoload method for PHPDebugr
  *  
- * @param string $class_name
+ * @param string $className
  */
-function PHPDebugr_autoload($class_name) {
-    $class_file_path = str_replace('_', '/', $class_name) . '.php';
-    $file = dirname(__FILE__) . '/' . $class_file_path;
-    if (true === file_exists($file)) {
-        require_once($file);
-        return true;
+function PHPDebugr_autoload($className) {
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require $fileName;
 }
 
 spl_autoload_register('PHPDebugr_autoload');
